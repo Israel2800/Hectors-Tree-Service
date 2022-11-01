ae6c74d9d1046236eff04f98f216d7edad4470f8
Dary <dady281100@gmail.com>
2022-11-01 00:05:18 -0500

    About, Navbar, Contact and Footer pages added

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About({ params }) {
+    return(
+        <section id="about">
+            <div className="">
+                Hectors Tree Service
+            </div>
+        </section>
+    )
+    
+}
