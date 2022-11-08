de6725d39ce1a7472b124c8192bade7a73ceaca2
Dary <dady281100@gmail.com>
2022-11-07 23:39:24 -0600

    Updating NavBar

--- src/components/pages/Navbar.js
+++ src/components/pages/Navbar.js
@@ -2,15 +2,45 @@ import React from 'react';
 
 export default function Navbar({ handlePageChange }) {
     return (
-        <header className=''>
-            <div className=''>
-               <a className=''>
-                    <a
-                        href='about' className=''
-                        onClick={() => handlePageChange('About')}>
-                        Hector's Tree Service
-                    </a>
-                </a> 
+        <header className="bg-gray-800 md:sticky top-0 z-10">
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <a className="title-font font-medium text-white mb-4 md:mb-0">
+                <a 
+                    href="#about" className="ml-3 text-xl"
+                    onClick={() => handlePageChange('About')}>
+                    Hector's Tree Service
+                </a>
+                </a>
+                <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+                {/* <a 
+                    href="#projects" className="mr-5 hover:text-white"
+                    onClick={() => handlePageChange('Projects')}>
+                    Projects
+                </a>
+                <a 
+                    href="#skills" className="mr-5 hover:text-white"
+                    onClick={() => handlePageChange('Skills')}>
+                    Skills
+                </a> */}
+                </nav>
+                <a 
+                href="#projects" className=" hover:text-white inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+                onClick={() => handlePageChange('Services')}>
+                Services
+                </a>
+                <a 
+                href="#skills" className=" hover:text-white inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+                
+                onClick={() => handlePageChange('Gallery')}>
+                Gallery
+                </a>
+                <a
+                href="#contact"
+                className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+                onClick={() => handlePageChange('Contact')}>
+                Contact Us!
+                {/* <ArrowRightIcon className="w-4 h-4 ml-1" /> */}
+                </a>
             </div>
         </header>
     );
