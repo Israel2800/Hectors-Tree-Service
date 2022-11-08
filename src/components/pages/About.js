21b490004558d60bbaf89075c9bf3ef6661d31b2
Dary <dady281100@gmail.com>
2022-11-07 23:25:40 -0600

    Adding information in the about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,10 +3,42 @@ import React from 'react';
 export default function About({ params }) {
     return(
         <section id="about">
-            <div className="">
-                Hectors Tree Service
-            </div>
-        </section>
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            HECTOR'S TREE SERVICE
+            <br className="lg:inline-block" />YOUR TREES DESERVE A PROFESSIONAL CARE
+          </h1>
+          <p className="mb-8 leading-relaxed text-justify">
+            Hector's Tree Service provides reliable tree care services in La Vergne, Brentwood, Smyrna & 
+            Mt. Juliet, TN
+          </p>
+
+          {/* <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-cyan-500 border-0 py-2 px-6 focus:outline-none hover:bg-cyan-700 rounded text-lg"
+              onClick={() => handlePageChange('Contact')}>
+              Contact Me
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+              onClick={() => handlePageChange('Projects')}>
+              See My Projects
+            </a>
+          </div> */}
+        </div>
+        {/* <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded-lg"
+            alt="hero"
+            // src={photo}
+            style={{width:"550px", height:"600px"}}
+          />
+        </div> */}
+      </div>
+    </section>
     );
     
 }
