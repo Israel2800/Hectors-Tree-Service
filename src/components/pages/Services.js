24c53b15efd8482441118b1b06a220d95c6d48c6
Dary <dady281100@gmail.com>
2022-11-11 00:06:22 -0600

    Dividing the information for Services.js

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -2,7 +2,16 @@ import React from "react";
 
 export default function Services() {
     return(
-        
+        <section className="">
+            <div className="">
+                <image>
+
+                </image>
+                <div>
+                    
+                </div>
+            </div>
+        </section>
 
     );
 }
