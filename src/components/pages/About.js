e91bd613b1315cf3951953982842a194394ae3d5
Dary <dady281100@gmail.com>
2022-11-12 23:55:27 -0600

    About.js file updated

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,14 +4,15 @@ export default function About({ params }) {
     return(
         <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            <img
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 flex flex-col  mb-16 md:mb-0 items-center text-center">
+        <img
                 className="object-cover object-center rounded-lg mb-8 leading-relaxed text-justify"
                 alt="hero"
                 src={HLogo}
                 style={{width:"200px", height:"200px"}}
             />
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            
           <p className="lg:inline-block">
             YOUR TREES DESERVE A PROFESSIONAL CARE
           </p>
