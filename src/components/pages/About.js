04e5fa7cf23248cab1a3c4e019135f13be99e4ea
Dary <dady281100@gmail.com>
2022-11-14 23:41:12 -0600

    Adding styles

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return(
       <section id="about">
         <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 flex flex-col  mb-16 md:mb-0 items-center text-center">
+          <div className="lg:flex-grow  flex flex-col  mb-16 md:mb-0 items-center text-center">
             <img
                     className="object-cover object-center rounded-lg mb-8 leading-relaxed text-justify"
                     alt="hero"
@@ -41,11 +41,11 @@ export default function About() {
         </div>
         {/* Second section */}
         <div className="flex px-10 py-20 md:flex-row flex-col items-center bg-white">
-          <div className="lg:flex-grow md:w-1/2 flex flex-col  mb-16 md:mb-0 items-center text-center">
+          <div className="lg:flex-grow flex flex-col mb-16 md:mb-0 items-center text-center">
             <p className='text-green-600 text-3xl font-serif'>
               We will maintain your space clean and beautiful
             </p> 
-            <p className='text-green-800 text-xl font-serif pt-2'>
+            <p className='text-green-800 text-xl font-serif mt-6'>
               Every tree that you own provides your property with beautiful scenery, shade and curb appeal. Shouldn't you make sure such prominent and useful landscape features are well cared for?
               To provide your tree with top-notch care, enlist a reputable tree care company. In La Vergne, Brentwood, Smyrna & Mt. Juliet, TN and surrounding areas, G Professional Tree Service is here to help. We'll give every tree the attentive services that it deserves.
             </p>
