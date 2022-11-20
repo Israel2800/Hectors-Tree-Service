b7571bc3a6393b9d13226cb31d0b9488e749578e
Dary <dady281100@gmail.com>
2022-11-19 21:40:09 -0600

    Updating Reviews.js to Testimonials.js

--- src/components/pages/Testimonials.js
+++ src/components/pages/Testimonials.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
-import { reviews } from "../../data";
+import { testimonials } from "../../data";
 
-export default function Reviews(){
+export default function testimonials(){
     return(
         <section id="reviews">
             <div className=" px-5 py-10 mx-auto text-center bg-slate-600">
@@ -11,23 +11,23 @@ export default function Reviews(){
                     Client Reviews
                     </h1>
                     <div className="flex flex-wrap m-4">
-                    {reviews.map((review) => (
+                    {testimonials.map((testimonial) => (
                         <div className="p-4 md:w-1/2 w-full">
                         <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
                             <TerminalIcon className="block w-8 text-gray-500 mb-4" />
-                            <p className="leading-relaxed mb-6">{review.quote}</p>
+                            <p className="leading-relaxed mb-6">{testimonial.quote}</p>
                             <div className="inline-flex items-center">
                             <img
                                 alt="testimonial"
-                                src={review.image}
+                                src={testimonial.image}
                                 className="w-12 rounded-full flex-shrink-0 object-cover object-center"
                             />
                             <span className="flex-grow flex flex-col pl-4">
                                 <span className="title-font font-medium text-white">
-                                {review.name}
+                                {testimonial.name}
                                 </span>
                                 <span className="text-gray-500 text-sm uppercase">
-                                {review.company}
+                                {testimonial.company}
                                 </span>
                             </span>
                             </div>
--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export const reviews = [
+export const testimonials = [
     {
         quote:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
