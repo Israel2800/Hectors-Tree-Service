db783a48a4042051de8348463d853c5ee9b78687
Dary <dady281100@gmail.com>
2022-11-24 00:12:29 -0600

    Adding two more services

---
+++ src/components/pages/services/TreeCare.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function TreeCare() {
+    return(
+        <section className="">
+            <div className="container">
+                <image>
+
+                </image>
+                <div>
+                    
+                </div>
+            </div>
+        </section>
+
+    );
+}
