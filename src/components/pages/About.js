45f3e8667c33c6539b0d0d99646b4fa34f17f505
Dary <dady281100@gmail.com>
2022-11-25 19:01:20 -0600

    Third section updated, still working on styles and user information for About.js file

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -65,33 +65,33 @@ export default function About({handlePageChange}) {
         
         {/* Third section */}
         <div className="px-0 py-0 grid md:grid-cols-2 bg-slate-600">
-          <div className="w-full h-72 items-center text-justify mx-0 my-0 px-5 py-5 bg-stumpRemoval bg-cover bg-center bg-no-repeat opacity-70 hover:opacity-60">
-            <p className='text-white text-3xl font-serif'>
-              We will maintain your space clean and beautiful
+          <div className="w-full items-center text-justify mx-0 my-0 px-12 py-14 bg-stumpRemoval bg-cover bg-center bg-no-repeat opacity-70 hover:opacity-60">
+            <p className='text-white text-3xl font-serif text-center'>
+              TREE REMOVAL
             </p> 
             <p className='text-white text-xl font-serif mt-6'>
               Every tree that you own provides your property with beautiful scenery, shade and curb appeal. Shouldn't you make sure such prominent and useful landscape features are well cared for?
             </p>
           </div>
-          <div className="w-full h-72 items-center text-justify mx-0 my-0 px-5 py-5 bg-stumpRemoval bg-cover bg-center bg-no-repeat opacity-70 hover:opacity-60">
-            <p className='text-white text-3xl font-serif'>
-              We will maintain your space clean and beautiful
+          <div className="w-full items-center text-justify mx-0 my-0 px-12 py-14 bg-stumpRemoval bg-cover bg-center bg-no-repeat opacity-70 hover:opacity-60">
+            <p className='text-white text-3xl font-serif text-center'>
+              TREE TRIMMING
             </p> 
             <p className='text-white text-xl font-serif mt-6'>
               Every tree that you own provides your property with beautiful scenery, shade and curb appeal. Shouldn't you make sure such prominent and useful landscape features are well cared for?
             </p>
           </div>
-          <div className="w-full h-72 items-center text-justify mx-0 my-0 px-5 py-5 bg-stumpRemoval bg-cover bg-center bg-no-repeat opacity-70 hover:opacity-60">
-            <p className='text-white text-3xl font-serif'>
-              We will maintain your space clean and beautiful
+          <div className="w-full items-center text-justify mx-0 my-0 px-12 py-14 bg-stumpRemoval bg-cover bg-center bg-no-repeat opacity-70 hover:opacity-60">
+            <p className='text-white text-3xl font-serif text-center'>
+              STUMP REMOVAL
             </p> 
             <p className='text-white text-xl font-serif mt-6'>
               Every tree that you own provides your property with beautiful scenery, shade and curb appeal. Shouldn't you make sure such prominent and useful landscape features are well cared for?
             </p>
           </div>
-          <div className="w-full h-72 items-center text-justify mx-0 my-0 px-5 py-5 bg-stumpRemoval bg-cover bg-center bg-no-repeat opacity-70 hover:opacity-60">
-            <p className='text-white text-3xl font-serif'>
-              We will maintain your space clean and beautiful
+          <div className="w-full items-center text-justify mx-0 my-0 px-12 py-14 bg-stumpRemoval bg-cover bg-center bg-no-repeat opacity-70 hover:opacity-60">
+            <p className='text-white text-3xl font-serif text-center'>
+              TREE CARE
             </p> 
             <p className='text-white text-xl font-serif mt-6'>
               Every tree that you own provides your property with beautiful scenery, shade and curb appeal. Shouldn't you make sure such prominent and useful landscape features are well cared for?
