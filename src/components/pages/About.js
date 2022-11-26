cd0c1aa9fd2a9a127bcd7ccbed9eee2889284834
Dary <dady281100@gmail.com>
2022-11-25 19:05:26 -0600

    Adding Tree Trimming image

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -73,7 +73,7 @@ export default function About({handlePageChange}) {
               Every tree that you own provides your property with beautiful scenery, shade and curb appeal. Shouldn't you make sure such prominent and useful landscape features are well cared for?
             </p>
           </div>
-          <div className="w-full items-center text-justify mx-0 my-0 px-12 py-14 bg-stumpRemoval bg-cover bg-center bg-no-repeat opacity-70 hover:opacity-60">
+          <div className="w-full items-center text-justify mx-0 my-0 px-12 py-14 bg-treeTrimming bg-cover bg-center bg-no-repeat opacity-70 hover:opacity-60">
             <p className='text-white text-3xl font-serif text-center'>
               TREE TRIMMING
             </p> 
