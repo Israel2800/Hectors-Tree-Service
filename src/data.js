3a94190b9ca3661240c4a6655c21a7a76af7236a
Dary <dady281100@gmail.com>
2022-11-27 23:27:49 -0600

    Cleaning code in data.js file

--- src/data.js
+++ src/data.js
@@ -20,8 +20,6 @@ export const services = [
     title: "TREE REMOVAL",
     image: require('./assets/images/tree-care.jpg'),
     description: "Every tree that you own provides your property with beautiful scenery, shade and curb appeal. Shouldn't you make sure such prominent and useful landscape features are well cared for?"
-
-    
   },
   {
     title: "TREE TRIMMING",
