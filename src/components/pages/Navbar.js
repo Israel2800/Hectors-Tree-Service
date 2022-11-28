bd5cd03b60924399096823b4ef79855d3024a52d
Dary <dady281100@gmail.com>
2022-11-28 01:47:41 -0600

    Adding StumpRemoval option to Navbar.js file

--- src/components/pages/Navbar.js
+++ src/components/pages/Navbar.js
@@ -29,12 +29,12 @@ export default function Navbar({ handlePageChange }) {
                     onClick={() => handlePageChange('Services')}>
                     Services
                 </a>
-                {/* <a 
-                    href="#gallery" className=" hover:text-white inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+                <a 
+                    href="#StumpRemoval" className=" hover:text-white inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
                     
-                    onClick={() => handlePageChange('Gallery')}>
-                    Gallery
-                </a> */}
+                    onClick={() => handlePageChange('StumpRemoval')}>
+                    Stump Removal
+                </a>
                 <a
                     href="#contact"
                     className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
