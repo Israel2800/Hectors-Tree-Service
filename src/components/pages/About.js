43b6eb5a735318a067ef918766832594b90bc1c4
Israel Aguilar <dady281100@gmail.com>
2022-11-29 23:35:38 -0600

    Cleaning code in About.js file

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -36,12 +36,7 @@ export default function About({handlePageChange}) {
                 onClick={() => handlePageChange('Contact')}>
                 Contact Me
               </a>
-              {/* <a
-                href="#projects"
-                className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
-                onClick={() => handlePageChange('Projects')}>
-                See My Projects
-              </a> */}
+      
             </div>
           </div>
           
