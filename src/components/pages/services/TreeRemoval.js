3c341f6639d43adaaec365b73db8aa58e6194b6c
Dary <dady281100@gmail.com>
2022-12-02 00:01:17 -0600

    Cleaning and testing img section for TreeRemoval.js file, still working on it

--- src/components/pages/services/TreeRemoval.js
+++ src/components/pages/services/TreeRemoval.js
@@ -4,8 +4,7 @@ export default function TreeRemoval() {
     return(
         <section className="">
             <div className="container">
-                <image className="absolute w-full h-full bg-treeRemoval bg-cover bg-no-repeat opacity-60">
-                </image>
+                <image className="absolute w-full h-full bg-treeRemoval bg-cover bg-no-repeat opacity-60"/>
                 <div>
                     
                 </div>
