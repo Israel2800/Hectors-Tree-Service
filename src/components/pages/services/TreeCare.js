8f96e1835ea117680d388e945eb33d693f098d7f
Dary <dady281100@gmail.com>
2022-12-08 23:38:14 -0600

    Content added to TreeCare.js file

--- src/components/pages/services/TreeCare.js
+++ src/components/pages/services/TreeCare.js
@@ -4,12 +4,38 @@ export default function TreeCare() {
     return(
         <section className="">
             <div className="container">
-                <image className="absolute w-full h-full bg-treeRemoval bg-cover bg-no-repeat opacity-60">
+            <div className="mx-auto flex px-10 py-20 md:flex-row flex-col items-center bg-mainImage bg-cover bg-fixed bg-center bg-no-repeat bg-opacity-75 hover:opacity-100">
+          <img className=""/>
+          <div className="lg:flex-grow flex flex-col mb-16 md:mb-0 items-center text-center ">
+            <img
+              className="object-cover object-center rounded-lg mb-8 leading-relaxed text-justify"
+              alt="hero"
+              src={HLogo}
+              style={{width:"200px", height:"200px"}}
+            />
+            <h1 className="sm:text-4xl text-3xl mb-4 font-medium text-white">
+              <p className="lg:inline-block">
+                YOUR TREES DESERVE A PROFESSIONAL CARE
+              </p>
+            </h1>
+            <p className="mb-8 leading-relaxed text-justify">
+              Hector's Tree Service provides reliable tree care services in La Vergne, Brentwood, Smyrna & 
+              Mt. Juliet, TN
+            </p>
 
-                </image>
-                <div>
-                    
-                </div>
+            <div className="flex justify-center">
+              <a
+                href="#contact"
+                className="inline-flex text-white bg-cyan-500 border-0 py-2 px-6 focus:outline-none hover:bg-cyan-700 rounded text-lg"
+                onClick={() => handlePageChange('Contact')}>
+                Contact Me
+              </a>
+      
+            </div>
+          </div>
+          
+
+        </div>
             </div>
         </section>
 
