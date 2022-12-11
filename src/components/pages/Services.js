689bdd0a7f5dba2ec50b73a280c50ddba198ad98
Israel Aguilar <dady281100@gmail.com>
2022-12-10 22:08:12 -0600

    Update Services.js

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -2,33 +2,3 @@
 
 import React from "react";
 import { services } from "../../data";
-
-
-// export default function Services() {
-//     return(
-//         <section id="services" className="">
-//             {services.map((service) => (
-//                 <a
-//                     key={service.image}
-//                     className="sm:w-1/2 w-full p-4" target="blank" rel="noreferrer"
-//                 >
-//                 <div className="flex relative">
-//                     <a
-//                         className="absolute  w-full items-center text-justify mx-0 my-0 px-12 py-14 bg-treeTrimming bg-cover bg-center bg-no-repeat opacity-60 hover:opacity-40"
-//                     />
-//                     <p className='relative text-white text-3xl font-serif text-center'>
-//                         TREE TRIMMING
-//                     </p> 
-//                     <p className='relative text-white text-xl font-serif mt-6'>
-//                         Every tree that you own provides your property with beautiful scenery, shade and curb appeal. Shouldn't you make sure such prominent and useful landscape features are well cared for?
-//                     </p>
-//                 </div>
-            
-//                 </a>
-//             ))}
-
-            
-//         </section>
-
-//     );
-// }
