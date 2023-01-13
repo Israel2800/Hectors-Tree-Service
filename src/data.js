747d466085178f6d11c6e559903db2a54fe90d48
Israel Aguilar <dady281100@gmail.com>
2023-01-12 23:54:40 -0600

    Tree Care service added to data.js

--- src/data.js
+++ src/data.js
@@ -26,4 +26,9 @@ export const services = [
     image: require('./assets/images/tree-trimming.jpg'),
     description: "Every tree that you own provides your property with beautiful scenery, shade and curb appeal. Shouldn't you make sure such prominent and useful landscape features are well cared for?"    
   },
+  {
+    title: "TREE CARE",
+    image: require('./assets/images/tree-trimming.jpg'),
+    description: "Every tree that you own provides your property with beautiful scenery, shade and curb appeal. Shouldn't you make sure such prominent and useful landscape features are well cared for?"    
+  },
 ]
