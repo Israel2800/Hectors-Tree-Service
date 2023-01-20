0f474ef0fe9a8b3ab62db10bb1a4398047d1d65b
Israel Aguilar <dady281100@gmail.com>
2023-01-19 23:16:48 -0600

    StumpRemoval.js file updated with information

--- src/components/pages/services/StumpRemoval.js
+++ src/components/pages/services/StumpRemoval.js
@@ -8,7 +8,15 @@ export default function StumpRemoval() {
                     
                 </image>
                 <div>
-                    
+                    <h1 className="sm:text-4xl text-3xl mb-4 font-medium text-white">
+                        <p className="lg:inline-block">
+                            YOUR TREES DESERVE A PROFESSIONAL CARE
+                        </p>
+                    </h1>
+                    <p className="mb-8 leading-relaxed text-justify">
+                        Hector's Tree Service provides reliable tree care services in La Vergne, Brentwood, Smyrna & 
+                        Mt. Juliet, TN
+                    </p>
                 </div>
             </div>
         </section>
