c7694d357d9bd7b3e71ac296f8cb06e59de7bfa3
Israel Aguilar <dady281100@gmail.com>
2023-01-21 21:47:59 -0600

    Update Trimming.js file with client information

--- src/components/pages/services/Trimming.js
+++ src/components/pages/services/Trimming.js
@@ -20,6 +20,16 @@ export default function Trimming() {
                         Hector's Tree Service provides reliable tree care services in La Vergne, Brentwood, Smyrna & 
                         Mt. Juliet, TN
                     </p>
+                    <h1>Tree Trimming Services</h1>
+                        <p>Welcome to our tree trimming services page. We provide professional and efficient tree trimming services for both residential and commercial properties. Our experienced team of arborists can help keep your trees healthy and looking great.</p>
+                        <h2>Our Services</h2>
+                        <ul>
+                          <li>Pruning and shaping of trees and shrubs</li>
+                          <li>Removal of dead or diseased branches</li>
+                          <li>Crown reduction and thinning</li>
+                          <li>Stump grinding and removal</li>
+                        </ul>
+                        <h2>Contact Us</h2>
                 </div>
             </div>
         </section>
