826fe0b3a646c8a186d6d7e563939a96c8fc9c06
Israel Aguilar <dady281100@gmail.com>
2023-03-27 23:44:08 -0500

    fixing typo in data.js

--- src/data.js
+++ src/data.js
@@ -28,7 +28,7 @@ export const services = [
   },
   {
     title: "TREE CARE",
-    image: require('./assets/images/tree-trimming.jpg'),
+    image: require('./assets/images/tree-care.jpg'),
     description: "Every tree that you own provides your property with beautiful scenery, shade and curb appeal. Shouldn't you make sure such prominent and useful landscape features are well cared for?"    
   },
 ]
